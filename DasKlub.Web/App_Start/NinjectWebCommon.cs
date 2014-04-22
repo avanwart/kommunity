e22e878f327592c20b3b77988864a21c9f66c57a
Ryan Williams <ryanmichaelwilliams@gmail.com>
2014-04-21 21:43:45 -0700

    nuget updates (ninject, ms etc.)

--- DasKlub.Web/App_Start/NinjectWebCommon.cs
+++ DasKlub.Web/App_Start/NinjectWebCommon.cs
@@ -5,8 +5,9 @@ namespace DasKlub.Web.App_Start
 {
     using System;
     using System.Web;
-    using DasKlub.Lib.Services;
+
     using Microsoft.Web.Infrastructure.DynamicModuleHelper;
+
     using Ninject;
     using Ninject.Web.Common;
 
@@ -60,7 +61,6 @@ namespace DasKlub.Web.App_Start
         /// <param name="kernel">The kernel.</param>
         private static void RegisterServices(IKernel kernel)
         {
-            kernel.Bind<IMailService>().To<MailService>();
         }        
     }
 }
